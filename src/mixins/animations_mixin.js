3f3c307b8e81f88d6a6597a26ce70c2fc80a560a
timmelcherson <tim.melcherson@gmail.com>
2021-01-22 11:18:05 +0100

    Removed commented code

--- src/mixins/animations_mixin.js
+++ src/mixins/animations_mixin.js
@@ -38,32 +38,9 @@ Vue.mixin({
         element.style.visibility = 'visible';
         element.style.transition = element.style.WebkitTransition = 'none';
 
-        
         if ('stroke-dasharray' in element.attributes) {
           element.style.stroke = dashOverlayColor;
         } else {
-          // window.getComputedStyle(document.querySelector('project-section-container'), null).getPropertyValue('background-color');
-        //   console.log(element.attributes);
-        // }
-        // if ('stroke-dasharray' in element.attributes) {
-        //   // element.style.strokeDasharray = element.attributes.strokeDasharray;
-        //   console.log(element.attributes);
-        //   element.style.opacity = "0";
-        //   element.style.transition = element.style.WebkitTransition = `opacity ${duration}ms ${animationType}`;
-        //   // element.style.opacity = "1";
-        //   // const dashLength = element.attributes['stroke-dasharray'].value.charAt(0)
-        //   // const dashOffset = element.attributes['stroke-dasharray'].value.charAt(2)
-        //   // element.style.strokeDasharray = `${0} ${length*2}`;
-        //   // element.style.strokeDashoffset = length;
-
-        //   // element.getBoundingClientRect();
-
-        //   // element.style.transition = element.style.WebkitTransition = `stroke-dasharray ${duration}ms ${animationType} ${delay}ms`;
-
-        //   // element.style.strokeDasharray = `${dashLength} ${dashOffset}`;
-        //   // element.style.strokeDashoffset = length;
-        //   // element.style.strokeDashoffset = dashOffset;
-        // } else {
           element.style.strokeDasharray = `${length} ${length}`;
           element.style.strokeDashoffset = length;
 
@@ -79,8 +56,6 @@ Vue.mixin({
           element.style.transition = element.style.WebkitTransition = `fill-opacity 300ms ${animationType} ${ANIM_TIME_MS}ms`;
           element.style.fillOpacity = 1;
         }
-
-        // console.log(`drawSvgPath with duration: ${ANIM_TIME_MS}ms`);
 
         return new Promise(resolve => {
           setTimeout(() => {
